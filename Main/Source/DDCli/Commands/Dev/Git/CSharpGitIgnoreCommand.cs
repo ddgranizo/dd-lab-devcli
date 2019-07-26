254acd645ad04bb4045bb2e050482c9802fb7fc7
Daniel Diaz Granizo <daniel.diazg@scmspain.com>
2019-07-26 10:38:59 +0200

    starting first command

---
+++ Main/Source/DDCli/Commands/Dev/Git/CSharpGitIgnoreCommand.cs
@@ -0,0 +1,19 @@
+﻿using DDCli.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDCli.Commands.Dev.Git
+{
+    public class CSharpGitIgnoreCommand : CommandBase
+    {
+
+        private const string HelpDefinition = "Downloads the file stored in ";
+
+
+        public CSharpGitIgnoreCommand()
+            :base(nameof(CSharpGitIgnoreCommand), typeof(CSharpGitIgnoreCommand).Namespace, HelpDefinition)
+        {
+        }
+    }
+}
---
+++ Main/Source/DDCli/Models/CommandBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDCli.Models
+{
+    public class CommandBase
+    {
+        public string CommandName { get; set; }
+        public string CommandNameSpace { get; set; }
+        public string Help { get; set; }
+        public CommandBase(string commandNameSpace, string commandName, string help)
+        {
+            if (string.IsNullOrEmpty(commandNameSpace))
+            {
+                throw new ArgumentException("message", nameof(commandNameSpace));
+            }
+
+            if (string.IsNullOrEmpty(commandName))
+            {
+                throw new ArgumentException("message", nameof(commandName));
+            }
+
+            if (string.IsNullOrEmpty(help))
+            {
+                throw new ArgumentException("message", nameof(help));
+            }
+
+            CommandNameSpace = commandNameSpace;
+            CommandName = commandName;
+            Help = help;
+        }
+    }
+}
